33750381051e4bc6e9ff6c4127df189de9d3f612
funkyduck <e@m.ail>
2020-07-16 04:34:42 -0400

    mkdir test

--- commands_mkdir_test.go
+++ commands_mkdir_test.go
@@ -1,11 +1,13 @@
 package main_test
 
 import (
+	"strings"
 	"testing"
 	main "github.com/mostfunkyduck/kp"
 )
 
 func TestMkdir(t *testing.T) {
+	// Happy path
 	r := createTestResources(t)
 	groupName := "test"
 	r.Context.Args = []string{
@@ -20,5 +22,14 @@ func TestMkdir(t *testing.T) {
 	expected := r.Db.CurrentLocation().Pwd() + groupName + "/" 
 	if l.Pwd() != expected {
 		t.Fatalf("[%s] != [%s]", l.Pwd(), expected)
+	}
+
+	r.F.outputHolder.output = ""
+	// Testing a duplicate while we're at it
+	main.NewGroup(r.Shell)(r.Context)
+	o := r.F.outputHolder.output
+	expected = "cannot create duplicate"
+	if !strings.Contains(o, expected) {
+		t.Fatalf("[%s] does not contain [%s]", o, expected)
 	}
 }
