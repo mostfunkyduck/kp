72618ccde32f684f1d10e0d8e098d89ba3eab8ce
funkyduck <e@m.ail>
2020-07-26 09:51:53 -0400

    moving stuff

--- keepass/tests/entry.go
+++ keepass/tests/entry.go
@@ -6,16 +6,6 @@ import (
 	k "github.com/mostfunkyduck/kp/keepass"
 )
 
-type Resources struct {
-	Db k.Database
-	Entry k.Entry
-	Group k.Group
-	// BlankEntry and BlankGroup are empty resources for testing freshly
-	// allocated structs
-	BlankEntry k.Entry
-	BlankGroup k.Group
-}
-
 func RunTestNoParent(t *testing.T, r Resources) {
 	name := "shmoo"
 	e := r.Entry
